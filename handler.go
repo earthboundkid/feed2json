05e6f0e681e8d6f582bc2a0fb94d5610ddc47dec
Carl Johnson <me@carlmjohnson.net>
2019-02-03 22:01:05 -0500

    Handler: Add middleware context system
    This will let me do an HTML page if the URL is invalid in feed2jsonweb.

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package feed2json
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"log"
 	"net/http"
@@ -54,8 +55,10 @@ type Middleware = func(http.Handler) http.Handler
 type Logger = func(format string, v ...interface{})
 
 // Handler is an http.Handler that extracts and validates a URL for a request,
-// then requests is with the provided http.Client. Responses from Handler are
-// wrapped by the user provided middleware, if any.
+// sets the URL and its validity with SetFeedURLContext. Responses from Handler
+// are then wrapped by the user provided middleware, if any. Finally, the innermost
+// handler requests valid URLs with the provided http.Client by unwrapping
+// FeedURLFromContext.
 //
 // c if nil defaults to http.DefaultClient. l if nil defaults to log.Printf.
 func Handler(x URLExtractor, v URLValidator, c *http.Client, l Logger, ms ...Middleware) http.Handler {
@@ -65,48 +68,61 @@ func Handler(x URLExtractor, v URLValidator, c *http.Client, l Logger, ms ...Mid
 	if l == nil {
 		l = log.Printf
 	}
-	var h http.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet && r.Method != http.MethodHead {
-			handleErr(w, l, http.StatusMethodNotAllowed,
-				"method %q not allowed", r.Method)
-			return
-		}
-		u := x(r)
-		if u == nil || !v(u) {
-			handleErr(w, l, http.StatusBadRequest,
-				"bad url requested: %q", u)
-			return
-		}
-		rsp, err := c.Get(u.String())
-		if err != nil {
-			handleErr(w, l, http.StatusBadGateway,
-				"error connecting to %q: %v", u, err)
-			return
-		}
-		defer rsp.Body.Close()
-		var from, to bytes.Buffer
-		if _, err = from.ReadFrom(rsp.Body); err != nil {
-			handleErr(w, l, http.StatusBadGateway,
-				"error reading %q: %v", u, err)
-			return
-		}
-		if err = Convert(&from, &to); err != nil {
-			handleErr(w, l, http.StatusInternalServerError,
-				"error converting %q: %v", u, err)
-			return
-		}
+	h := http.Handler(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodGet && r.Method != http.MethodHead {
+				handleErr(w, l, http.StatusMethodNotAllowed,
+					"method %q not allowed", r.Method)
+				return
+			}
+			u, ok := FeedURLFromContext(r.Context())
+			if !ok {
+				handleErr(w, l, http.StatusBadRequest,
+					"bad url requested: %q", u)
+				return
+			}
+			rsp, err := c.Get(u.String())
+			if err != nil {
+				handleErr(w, l, http.StatusBadGateway,
+					"error connecting to %q: %v", u, err)
+				return
+			}
+			defer rsp.Body.Close()
+			var from, to bytes.Buffer
+			if _, err = from.ReadFrom(rsp.Body); err != nil {
+				handleErr(w, l, http.StatusBadGateway,
+					"error reading %q: %v", u, err)
+				return
+			}
+			if err = Convert(&from, &to); err != nil {
+				handleErr(w, l, http.StatusInternalServerError,
+					"error converting %q: %v", u, err)
+				return
+			}
 
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		if _, err = io.Copy(w, &to); err != nil {
-			l("error completing write of %q: %v", u, err)
-			return
-		}
-		l("[%d] converted %q", http.StatusOK, u)
-	})
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			if _, err = io.Copy(w, &to); err != nil {
+				l("error completing write of %q: %v", u, err)
+				return
+			}
+			l("[%d] converted %q", http.StatusOK, u)
+		}))
+
+	ms = append([]Middleware{addFeedURLContext(x, v)}, ms...)
 	for _, m := range ms {
 		h = m(h)
 	}
 	return h
+}
+
+func addFeedURLContext(x URLExtractor, v URLValidator) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			u := x(r)
+			r = r.WithContext(SetFeedURLContext(r.Context(), u, v(u)))
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 func handleErr(w http.ResponseWriter, l Logger, code int, format string, v ...interface{}) {
@@ -114,4 +130,28 @@ func handleErr(w http.ResponseWriter, l Logger, code int, format string, v ...in
 	http.Error(w, statusText, code)
 	v = append([]interface{}{code}, v...)
 	l("[%d] "+format, v...)
+}
+
+type feedURLContextKeyType byte
+
+const feedURLContextKey feedURLContextKeyType = iota
+
+type feedURLContextValueType struct {
+	*url.URL
+	bool
+}
+
+// SetFeedURLContext allows middleware to intercept Handler calls and change
+// the feed URL or its validity.
+func SetFeedURLContext(ctx context.Context, u *url.URL, valid bool) context.Context {
+	return context.WithValue(ctx, feedURLContextKey, feedURLContextValueType{u, valid})
+}
+
+// FeedURLFromContext allows middleware to intercept the URLs and their
+// validity in Handler.
+func FeedURLFromContext(ctx context.Context) (u *url.URL, valid bool) {
+	if v, ok := ctx.Value(feedURLContextKey).(feedURLContextValueType); ok {
+		return v.URL, v.bool
+	}
+	return nil, false
 }
